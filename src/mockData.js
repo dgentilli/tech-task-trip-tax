3e709de4d30a0fdbde42b4e4bad01a27fe24bdf9
Dave Gentilli <dgentilli@gmail.com>
2021-09-14 15:22:57 +0300

    Create mock data

---
+++ src/mockData.js
@@ -0,0 +1,18 @@
+export const mockSalesDataByPerson = [
+  {
+    employeeName: 'Ionut',
+    numSales: 26,
+    revenue: 6000,
+  },
+  {
+    employeeName: 'Elena',
+    numSales: 23,
+    revenue: 5500,
+  },
+  { employeeName: 'Radu', numSales: 19, revenue: 3500 },
+  {
+    employeeName: 'Mihaela',
+    numSales: 22,
+    revenue: 4500,
+  },
+];
