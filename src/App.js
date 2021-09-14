1baabc7a31de04e7c5dbe2f3fed52e39c9692056
Dave Gentilli <dgentilli@gmail.com>
2021-09-14 10:24:39 +0300

    Remove unnecessary CRA files and setup initial routing.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Reports from './components/Reports';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact>
+            <Dashboard />
+          </Route>
+          <Route path='/reports' exact>
+            <Reports />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
