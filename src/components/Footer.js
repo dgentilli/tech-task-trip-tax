fd128b912bdbff6164882093c32318039df50fac
Dave Gentilli <dgentilli@gmail.com>
2021-09-14 11:19:16 +0300

    Implement Footer with navigation.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-function Footer() {
-  return <div>Footer</div>;
-}
+import theme from '../theme';
+import { NavMenuItemsWrapper } from '../globalStyles/navigationStyles';
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  height: 5rem;
+  background-color: ${theme.color.BG_DARK};
+  color: ${theme.color.TEXT_LIGHT}
+  margin-top: 1rem;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <NavMenuItemsWrapper>
+        <Link
+          to='/'
+          style={{
+            textDecoration: 'none',
+            color: `${theme.color.BG_LIGHT}`,
+            fontSize: `0.9rem`,
+          }}
+        >
+          Dashboard
+        </Link>
+        <Link
+          to='/reports'
+          style={{
+            textDecoration: 'none',
+            color: `${theme.color.BG_LIGHT}`,
+            fontSize: `0.9rem`,
+          }}
+        >
+          Reports
+        </Link>
+      </NavMenuItemsWrapper>
+    </FooterWrapper>
+  );
+};
 
 export default Footer;
