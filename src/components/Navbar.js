92929c63e9a015876dabf88cf207aabd0f7af769
Dave Gentilli <dgentilli@gmail.com>
2021-09-14 11:01:17 +0300

    Implement top nav bar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import theme from '../theme';
+import { NavMenuItemsWrapper } from '../globalStyles/navigationStyles';
+
+const NavWrapper = styled.div`
+  position: sticky;
+  top: 0;
+  background-color: ${theme.color.SKY_BLUE};
+  height: 5rem;
+  width: 100%;
+  z-index: 3;
+`;
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <NavWrapper>
+      <NavMenuItemsWrapper>
+        <Link
+          to='/'
+          style={{
+            textDecoration: 'none',
+            color: `${theme.color.TEXT_LIGHT}`,
+            fontSize: `1.5rem`,
+          }}
+        >
+          TripTax
+        </Link>
+        <Link
+          to='/reports'
+          style={{
+            textDecoration: 'none',
+            color: `${theme.color.TEXT_LIGHT}`,
+            fontSize: `0.9rem`,
+          }}
+        >
+          Reports
+        </Link>
+      </NavMenuItemsWrapper>
+    </NavWrapper>
+  );
 }
 
 export default Navbar;
---
+++ src/globalStyles/navigationStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const NavMenuItemsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  height: 100%;
+  font-weight: 500;
+  font-size: 1.1rem;
+`;
