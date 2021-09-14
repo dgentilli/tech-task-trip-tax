379885e3d05c354aea48a7fb3d69e22d7a499775
Dave Gentilli <dgentilli@gmail.com>
2021-09-14 10:52:48 +0300

    Create theme file for consistency/reusability.

---
+++ src/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+  color: {
+    BOX_SHADOW: '#CCC',
+    BG_LIGHT: '#f8f9faff',
+    BG_MEDIUM: '#f8f9faff',
+    BG_DARK: '#343a40ff',
+    TEXT_DARK: '#495057ff',
+    TEXT_LIGHT: '#f8f9faff',
+    SKY_BLUE: '#4cc9f0ff',
+    LIGHT_GRAY: '#ced4daff',
+    PINK: '#f72585ff',
+  },
+  fontWeight: {
+    LIGHT: '300',
+    MEDIUM: '400',
+    REGULAR: '500',
+    SEMIBOLD: '600',
+    BOLD: '700',
+  },
+  breakPoint: {
+    XSMALL: '320',
+    SMALL: '768',
+    LARGE: '1100',
+  },
+};
+
+export default theme;
