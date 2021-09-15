5f113ec82010c3fae06077cb5cb83a6475f8e7b7
Dave Gentilli <dgentilli@gmail.com>
2021-09-15 09:47:16 +0300

    Rearrange mock data to make sorting changes more apparent.

--- src/mockData.js
+++ src/mockData.js
@@ -1,13 +1,13 @@
 export const mockSalesDataByPerson = [
   {
-    employeeName: 'Ionut',
-    numSales: 26,
-    revenue: 6000,
-  },
-  {
     employeeName: 'Elena',
     numSales: 23,
     revenue: 5500,
+  },
+  {
+    employeeName: 'Ionut',
+    numSales: 26,
+    revenue: 6000,
   },
   { employeeName: 'Radu', numSales: 19, revenue: 3500 },
   {
